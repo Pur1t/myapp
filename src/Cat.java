0a493d511d58261ee81568fb4f421984dabd2db1
Pur1t <purit.te@ku.th>
2023-07-09 16:14:14 +0700

    Update Cat.java on feature2 branch

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,14 @@
 public class Cat {
+    String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat() {
+        this.name = "john";
+    }
+
     public void Purr() {
         System.out.println("meow~~ 🐈");
     }
